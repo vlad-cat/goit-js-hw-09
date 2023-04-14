4184614716217d7e018a6a6eed3cc33709953da8
vlad-cat <babenko.vlad1327@gmail.com>
2023-04-14 15:36:28 +0300

    my first work

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,59 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
 
+let timerId = null;
+
+btnStart.addEventListener('click', onStart);
+
+btnStop.addEventListener('click', onStop);
+
+function onStart() {
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+
+  timerId = setInterval(() => {
+    let color = getRandomHexColor();
+    document.body.style.background = color;
+  }, 1000);
+}
+
+function onStop() {
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+  clearInterval(timerId);
+  // document.body.style.background = 'white';
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+// const btnStart = document.querySelector('button[data-start]');
+// const btnStop = document.querySelector('button[data-stop]');
+
+// let timerId = null;
+
+// btnStart.addEventListener('click', onStart);
+
+// btnStop.addEventListener('click', onStop);
+
+// function onStart() {
+//   btnStart.disabled = true;
+//   btnStop.disabled = false;
+//   timerId = setInterval(() => {
+//     let color = getRandomHexColor();
+//     document.body.style.background = color;
+//   }, 1000);
+// }
+
+// function onStop() {
+//   btnStart.disabled = true;
+//   btnStop.disabled = false;
+//   clearInterval(timerId);
+// }
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
