7c6da3f4fc9e91d4b469953bd2929e5f8b191632
vlad-cat <115999694+vlad-cat@users.noreply.github.com>
2023-04-17 21:11:25 +0000

    Deploying to gh-pages from @ vlad-cat/goit-js-hw-09@1ed8da8e91b2e7e6877ede3693de59fcc5347090 🚀

---
+++ 01-color-switcher.bb557211.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]");console.log(t);const e=document.querySelector("button[data-stop]");let o=null;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,o=setInterval((()=>{let t=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(o)}));
+//# sourceMappingURL=01-color-switcher.bb557211.js.map
